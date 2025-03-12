1a00c5d8b0da6a2fe305659fbd6dba3a63956bf7
cupnoodles7 <akritikhetan7.sk@gmail.com>
2025-03-13 02:10:37 +0530

    initial import, basic bot

---
+++ pomo_dorito_bot.py
@@ -0,0 +1,29 @@
+# This example requires the 'message_content' intent.
+
+
+import discord
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+client.run(os.environ['BOT_TOKEN'])
