02f9c788052f6c8dcee1fdce61145214bf747ec9
cupnoodles7 <akritikhetan7.sk@gmail.com>
2025-05-11 21:56:42 +0530

    break timer added

--- pomobot/__main__.py
+++ pomobot/__main__.py
@@ -14,7 +14,8 @@ load_dotenv()
 intents = discord.Intents.default()
 intents.message_content = True
 
-bot = commands.Bot(command_prefix='!', help_command = None, intents=intents)
+bot = commands.Bot(command_prefix='!', help_command=None, intents=intents)
+
 
 async def main():
     async with bot:
--- pomobot/timer.py
+++ pomobot/timer.py
@@ -1,32 +1,33 @@
 from enum import Enum
+
+
 class TimerStatus(Enum):
     INITIALIZED = 1
     RUNNING = 2
     STOPPED = 3
     EXPIRED = 4
-    
+
 
 class Timer:
-    def __init__(self, max_ticks = 5):
+    def __init__(self, max_ticks=5):
         self.running = False
         self.status = TimerStatus.INITIALIZED
         self.ticks = 0
-        
-        
+
     def get_status(self):
         return self.status
-    
+
     def start(self, max_ticks):
-        self.max_ticks = max_ticks #25*60
+        self.max_ticks = max_ticks  # 25*60
         self.status = TimerStatus.RUNNING
         self.ticks = 0
-        
+
     def stop(self):
         self.status = TimerStatus.STOPPED
-    
+
     def get_ticks(self):
         return self.ticks
-    
+
     def tick(self):
         self.ticks += 1
         if self.get_ticks() >= self.max_ticks:
